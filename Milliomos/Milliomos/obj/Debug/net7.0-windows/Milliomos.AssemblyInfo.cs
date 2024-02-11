dbdf57a1dead04d5dc840449b33929a23fe82041
jdomi <jdomi@gmail.com>
2024-02-11 12:24:29 +0100

    Experted Design

--- Milliomos/Milliomos/obj/Debug/net7.0-windows/Milliomos.AssemblyInfo.cs
+++ Milliomos/Milliomos/obj/Debug/net7.0-windows/Milliomos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+691dc262e6125df17a515566262a9e439cc0a7f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b9715a35887492de9c490330ea973bf3563b2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
