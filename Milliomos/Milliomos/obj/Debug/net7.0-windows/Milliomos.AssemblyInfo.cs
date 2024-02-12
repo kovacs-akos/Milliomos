0d643ea6b712486b57a3cd59f3a2525cecfdb76b
jdomi <jdomi@gmail.com>
2024-02-12 20:41:20 +0100

    Translation Update

--- Milliomos/Milliomos/obj/Debug/net7.0-windows/Milliomos.AssemblyInfo.cs
+++ Milliomos/Milliomos/obj/Debug/net7.0-windows/Milliomos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a129f0be6e24f9b59287cbe0c33cdb4054c0d28e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f211b0ffa4d9b744517b6c75be3f9f534b7a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milliomos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
